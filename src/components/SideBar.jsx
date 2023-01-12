8715bbba0fab577872559589070294abfab924d0
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-12 22:38:48 +0100

    create sidebar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,47 @@
+import { Stack } from "@mui/system";
+import React from "react";
+import { categories } from "../utils/constants";
+
+const selectedCategory = "New";
+
+const SideBar = () => {
+   return (
+      <Stack
+         direction="row"
+         sx={{
+            overflowY: "auto",
+            height: { sx: "auto", md: "95%" },
+            flexDirection: { md: "column" },
+         }}
+      >
+         {categories.map((category) => {
+            return (
+               <button
+                  key={category.name}
+                  className="category-btn"
+                  style={{
+                     background:
+                        category.name === selectedCategory && "#fc150c",
+                     color: "white",
+                  }}
+               >
+                  <span
+                     style={{
+                        color:
+                           category.name === selectedCategory ? "white" : "red",
+                        marginRight: "15px",
+                     }}
+                  >
+                     {category.icon}
+                  </span>
+                  <span
+                     style={{opacity:category.name === selectedCategory  ? '1' : '0.8'}}
+                  >{category.name}</span>
+               </button>
+            );
+         })}
+      </Stack>
+   );
+};
+
+export default SideBar;
