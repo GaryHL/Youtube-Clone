30fe492f0ae8e8d9de26a6aaf4439b73219a6363
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-15 21:50:51 +0100

    create videos compontent

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { Stack, Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
 
-const Videos = ({videos}) => {
-    console.log(videos)
-  return (
-    <div>Videos</div>
-  )
-}
+const Videos = ({ videos }) => {
+   return (
+      <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+         {videos.map((item, index) => (
+            <Box key={index}>
+                {item.id.videoId && <VideoCard video={item}/>}
+                {item.id.channelId && <ChannelCard channelDetail={item}/>}
+            </Box>
+         ))}
+      </Stack>
+   );
+};
 
-export default Videos
+export default Videos;
