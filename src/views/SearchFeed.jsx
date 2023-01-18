1914dbee055e75c412c585f8144ed9662801b392
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-18 09:20:03 +0100

    feat: :white_check_mark: create Search Feed view
    Add new View, Create new petición and reused videos component

--- src/views/SearchFeed.jsx
+++ src/views/SearchFeed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import React, { useState, useEffect } from "react";
+import {useParams} from 'react-router-dom'
+import SideBar from "../components/SideBar";
+import Videos from "../components/Videos";
+import { fetchApiVideos } from "../utils/fetchApi";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+   const [videos, setVideos] = useState([]);
 
-export default SearchFeed
+   const {searchTerm} = useParams();
+   useEffect(() => {
+      fetchApiVideos(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items));
+   }, [searchTerm]);
+
+   return ( 
+         <Box p={2} sx={{ overflowY: "auto", height: "92vh", flex: 2 }}>
+            <Typography
+               variant="h4"
+               fontWeight="bold"
+               mb={2}
+               sx={{ color: "white" }}
+            >
+               Search Results for: 
+               <span style={{ color: "#f31503" }}> {searchTerm} Videos</span>
+            </Typography>
+
+            <Videos videos={videos} />
+         </Box>
+   );
+   }
+export default SearchFeed;
