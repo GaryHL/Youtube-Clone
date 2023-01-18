f4aee5f438ec0a2760a070dd6638cf372f10a9fd
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-18 09:18:58 +0100

    feat: :white_check_mark: create channel deatil view
    I created a new api request, and reused the videos component

--- src/views/ChannelDetail.jsx
+++ src/views/ChannelDetail.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/system";
+
+import Videos from "../components/Videos";
+import ChannelCard from "../components/ChannelCard";
+import { fetchApiVideos } from "../utils/fetchApi";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+   const { id } = useParams();
+   const [channelDetail, setChannelDetail] = useState(null);
+   const [videos, setVideos] = useState([]);
 
-export default ChannelDetail
+   useEffect(() => {
+      fetchApiVideos(`channels?part="snippet&id=${id}`).then((data) =>
+         setChannelDetail(data?.items[0])
+      );
+
+      fetchApiVideos(`search?channelId=${id}&part=snippet&order=date`).then(
+         (data) => setVideos(data?.items)
+      );
+   }, [id]);
+   console.log('videos', videos)
+
+   return (
+      <Box minHeight="95vh">
+         <Box>
+            <div
+               style={{
+                  background: `linear-gradient(43deg, rgba(53,1,113,1) 0%, rgba(171,37,37,1) 100%)`,
+                  zIndex: 10,
+                  height: "250px",
+               }}
+            ></div>
+            <ChannelCard channelDetail={channelDetail} marginTop={"-110px"} />
+         </Box>
+         <Box display='flex' p="2">
+          <Box sx={{mr:{sm:'100px'}}}/>
+            <Videos videos={videos}></Videos>
+         </Box>
+      </Box>
+   );
+};
+
+export default ChannelDetail;
