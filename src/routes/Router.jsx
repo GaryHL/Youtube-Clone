0458b1b28d56380dc6fdf70e060888c25d19250a
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-01-28 21:29:51 +0100

    delete exact path

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -14,7 +14,7 @@ const Router = () => {
 
       <Navbar/>
          <Routes>
-            <Route exact path="/" element={<Feed />} />
+            <Route path="/" element={<Feed />} />
             <Route path="/video/:id" element={<VideoDetail />} />
             <Route path="/channel/:id" element={<ChannelDetail />} />
             <Route path="/search/:searchTerm" element={<SearchFeed />} />
