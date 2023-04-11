aad47a078bb3e9f62196df456880475492462528
Gary HL <garyhl321@gmail.com>
2023-04-11 12:13:18 +0200

    add new categories

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -11,13 +11,14 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import ChatIcon from '@mui/icons-material/Chat';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
-  { name: 'Nuevo', icon: <HomeIcon />, },
+  { name: 'Nuevos', icon: <HomeIcon />, },
   { name: 'Midudev', icon: <CodeIcon />, },
-  { name: 'Tailwind', icon: <CodeIcon />, },
+  { name: 'Chat gpt', icon: <ChatIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Lofi', icon: <MusicNoteIcon /> },
